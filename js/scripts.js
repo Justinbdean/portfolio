83a3a2961d6db2414e6b5ab238ba816f211d2d39
Justin <165987898+Justinbdean@users.noreply.github.com>
2024-10-04 20:54:06 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// dark mode toggle funciton
+document.querySelector("#darkbutton").addEventListener('click', toggle);
+    // i used document.queryselectory to specify the id button "darkbutton" and added an event listener to listen for when clicked to start the toggle function
+var document
+
+function toggle () {
+    const body = document.body
+    body.classList.toggle("dark-mode")
+};
+
+
+
+
+
